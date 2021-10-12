67b2301e9fbda2cb7a8e5cadc0a37f3897f62c92
jfr2102 <johannes@friedlein.eu>
2021-10-12 17:51:51 +0200

    copy storm errors

--- buildpipeline.sh
+++ buildpipeline.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 ssh ubuntu@172.24.38.172 "cd storm-cluster/buildscripts; ./pipeline.sh;"
-python -m webbrowser "http://vm.johannes-friedlein.de/dashboard.html"
+python -m webbrowser "http://172.24.38.172/dashboard.html"
 sleep 1m
 ./testdriver.sh
 #TODO: fault injection..
-sleep 30m
+sleep 15m
+./errors.sh
+sleep 15m
 ./logs.sh
