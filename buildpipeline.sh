875f18f326640a224bfde81f06fba1210608dc0d
jfr2102 <johannes@friedlein.eu>
2021-10-13 22:21:41 +0200

    fetch prometheus metrics to file

--- buildpipeline.sh
+++ buildpipeline.sh
@@ -4,8 +4,9 @@ python -m webbrowser "http://172.24.38.172/dashboard.html"
 sleep 1m
 ./testdriver.sh
 #TODO: fault injection..
-sleep 15m
+sleep 20m
 ssh ubuntu@172.24.38.172 "cd storm-cluster/buildscripts; ./deploy_check_topology.sh;"
 sleep 15m
 ./errors.sh
 ./logs.sh
+python3 fetch-prom-metrics/fetch.py
